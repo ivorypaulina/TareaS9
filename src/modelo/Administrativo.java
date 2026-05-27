adda91300cd264e0303810a28d5e9de7f887b5e8
ivorypaulina <ivorypaulina10@gmail.com>
2026-05-26 22:44:09 -0500

    Arreglos administrativo

--- src/modelo/Administrativo.java
+++ src/modelo/Administrativo.java
@@ -1,5 +1,4 @@
 package modelo;
-
 public class Administrativo extends Empleado {
     private String departamento;
     private int horasTrabajadas;
@@ -7,7 +6,6 @@ public class Administrativo extends Empleado {
 
     public Administrativo(String cedula, String nombre, int edad, String telefono, String correo,
                           String departamento, int horasTrabajadas, double valorHora) {
-
         super(cedula, nombre, edad, telefono, correo);
         this.departamento = departamento;
         this.horasTrabajadas = horasTrabajadas;
@@ -22,13 +20,9 @@ public class Administrativo extends Empleado {
     @Override
     public void mostrarInformacion() {
         super.mostrarInformacion();
-
         System.out.println("Departamento: " + departamento);
-
         System.out.println("Horas trabajadas: " + horasTrabajadas);
-
         System.out.println("Valor por hora: " + valorHora);
-
         System.out.println("Pago: " + calcularPago());
     }
 }
